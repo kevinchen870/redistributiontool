eba2b2c812fda323bb5b436636fb77e75de9e4fc
kevinchen870 <kevinchen870@gmail.com>
2023-11-14 23:32:00 +1100

    editmap approach

--- editmap approach.R
+++ editmap approach.R
@@ -11,7 +11,7 @@ library(shinyscreenshot)
 library(htmlwidgets)
 library(mapedit)
 
-
+sf::sf_use_s2(FALSE)
 #Read in data
 SA1 = readRDS('files/SA1.rds')
 
@@ -77,39 +77,87 @@ pal <- colorBin(
   bins = 5)
 
 
+selected = editMap(leaflet() %>%
+                     addTiles() %>%
+                     #Add divisions with deviation palette
+                     addPolygons(data= shp_CED,
+                                 fillOpacity =0.1, fillColor = ~pal(shp_CED$deviation_2028),
+                                 weight = 5, stroke = TRUE, color = 'white',
+                                 group = 'Current Divisions',
+                                 label = lapply(paste0("Division: ",
+                                                       str_to_title(shp_CED$CED_NAME21),'<br>',
+                                                       "Projected Population: ",
+                                                       shp_CED$tot_project,
+                                                       '<br>',
+                                                       "Deviation: ",shp_CED$deviation_2028),
+                                                htmltools::HTML),
+                                 highlightOptions = highlightOptions(color = "white",
+                                                                     weight = 2,
+                                                                     bringToFront = FALSE)) %>%
+                     #Add SA2 base layer
+                     addPolygons(data = SA1,
+                                 fillColor = "white",
+                                 fillOpacity = 0.3,
+                                 color = "black",
+                                 stroke = TRUE,
+                                 weight = 1,
+                                 layerId = ~sa1_code_2021,
+                                 group = "SA1",
+                                 label = ~sa1_code_2021)) %>%
+  # Layers control
+  addLayersControl(
+    overlayGroups = c("Current Divisions", "SA1"),
+    options = layersControlOptions(collapsed = TRUE)) %>%
+  #Add deviation legend
+  addLegend(values = shp_CED$deviation_2028,position = 'bottomright',
+            title = 'deviation from projected (%)',
+            colors = c("#D53E4F", "#FC8D59", "#FEE08B", 
+                       "#E6F598", "#99D594", "#3288BD"),
+            labels = c('-8% - -6%',
+                       '-6%- -4%',
+                       '-4% - -2%',
+                       '-2% - 0%',
+                       '0% - 2%',
+                       '2% - 4%'))
+
+st_is_valid(selected$finished)
+st_make_valid(selected$finished)
+Goldstein= st_intersection(SA1,st_make_valid(selected$finished))
+plot(Goldstein['Division'])
 #Define UI of app
 ui = fluidPage(
   
   titlePanel('Redistribution Tool v0.1'),
   "Click on map to build from SA1",
   
-  selectizeInput(inputId = "Division",
-              label = "Select Division",
-              choices = sort(unique(Popdata$Division)), 
-              selected = NULL,
-              multiple = TRUE,
-              width = '80%'),
+  # selectizeInput(inputId = "Division",
+  #                label = "Select Division",
+  #                choices = sort(unique(Popdata$Division)), 
+  #                selected = NULL,
+  #                multiple = TRUE,
+  #                width = '80%'),
   tags$div(tags$br()),
-  leafletOutput("map"),
+  #leafletOutput("map"),
+  editModUI('editor'),
   tags$div(tags$br()),
   actionButton("screenshot", "Save current map view"),
   tags$div(tags$br()),
-  selectizeInput(inputId = "selected_locations",
-                 label = "Selected SA1s:",
-                 choices = SA1$SA1_CODE21,
-                 selected = NULL,
-                 multiple = TRUE,
-                 width = '80%'),
-  tags$div(tags$br()),
-  actionButton("ClearSelection", "Clear all"),
-  tags$div(tags$br()),
+  # selectizeInput(inputId = "selected_locations",
+  #                label = "Selected SA1s:",
+  #                choices = SA1$SA1_CODE21,
+  #                selected = NULL,
+  #                multiple = TRUE,
+  #                width = '80%'),
+  # tags$div(tags$br()),
+  # actionButton("ClearSelection", "Clear all"),
+  # tags$div(tags$br()),
   
   'List of SA1s being used',
   
   dataTableOutput("SA1.table"),
-  'Total projected population of SA1s selected',
-  tags$div(tags$br()),
-  dataTableOutput("Total.table"),
+  # 'Total projected population of SA1s selected',
+  # tags$div(tags$br()),
+  # dataTableOutput("Total.table"),
   
   tags$footer(
     class = "footer",
@@ -124,18 +172,18 @@ ui = fluidPage(
 #Server functions
 
 server <- function(input, output, session){
-
-    #create empty vector to hold all click ids
+  
+  #create empty vector to hold all click ids
   selected_ids <- reactiveValues(ids = vector())
-
-    
+  
+  
   #initial map output
-  output$map <- renderLeaflet({
-    leaflet() %>%
+  edits <- callModule(editMod, "editor",
+      leaflet() %>%
       addTiles() %>%
       #Add divisions with deviation palette
       addPolygons(data= shp_CED, 
-                  fillOpacity =0.8, fillColor = ~pal(shp_CED$deviation_2028),
+                  fillOpacity =0.2, fillColor = ~pal(shp_CED$deviation_2028),
                   weight = 5, stroke = TRUE, color = 'white',
                   group = 'Current Divisions', 
                   label = lapply(paste0("Division: ",
@@ -158,21 +206,10 @@ server <- function(input, output, session){
                   layerId = ~sa1_code_2021,
                   group = "SA1",
                   label = ~sa1_code_2021) %>%
-      #Add clickable SA2s as a group 
-      addPolygons(data = SA1,
-                  fillColor = "red",
-                  fillOpacity = 0.5,
-                  weight = 1,
-                  color = "black",
-                  stroke = TRUE,
-                  layerId = ~sa1_7code_2021,
-                  group = ~sa1_code_2021) %>%
       # Layers control
       addLayersControl(
         overlayGroups = c("Current Divisions", "SA1"),
         options = layersControlOptions(collapsed = TRUE)) %>%
-      #Hide clickable groups
-      hideGroup(group = SA1$sa1_code_2021)  %>%
       #Add deviation legend
       addLegend(values = shp_CED$deviation_2028,position = 'bottomright',
                 title = 'deviation from projected (%)',
@@ -183,93 +220,13 @@ server <- function(input, output, session){
                            '-4% - -2%',
                            '-2% - 0%',
                            '0% - 2%',
-                           '2% - 4%'))
-  }) #END RENDER LEAFLET
+                           '2% - 4%')))
   
-  #define leaflet proxy for second level map
-  proxy <- leafletProxy("map")
-  
-  #create empty vector to hold all click ids
-  selected <- reactiveValues(groups = vector())
-  
-  #Observe clicked event
-  observeEvent(input$map_shape_click, {
-    #If clicked on SA1 layer
-    if(input$map_shape_click$group == "SA1"){
-      selected$groups <- c(selected$groups, input$map_shape_click$id)
-      proxy %>% showGroup(group = input$map_shape_click$id)
-    } 
-    #If base division layer then do nothing
-    else if (input$map_shape_click$group =='Current Divisions') {
-      NULL
-    } 
-    #see if any difference
-    else {
-      selected$groups <- setdiff(selected$groups, input$map_shape_click$group)
-      proxy %>% hideGroup(group = input$map_shape_click$group)
-    }
-    #update selected inputs
-    updateSelectizeInput(session,
-                         inputId = "selected_locations",
-                         choices = SA1$sa1_code_2021,
-                         selected = selected$groups,
-                         server = TRUE)
-  })
-  
-  ##### Filter Division ####
-  
-  observeEvent(input$Division, {
-    SA1_select = SA1 %>% filter(Division %in% input$Division)
-    updateSelectizeInput(session,
-                         inputId = "selected_locations",
-                         choices = SA1$sa1_code_2021,
-                         selected = SA1_select$sa1_code_2021,
-                         server = TRUE)
-    
-    selected$groups <- c(selected$groups, SA1_select$sa1_code_2021)
-    proxy %>% showGroup(group = selected$groups)
-  })
-  
-  #observe screenshot events
-  observeEvent(input$screenshot, {
-    screenshot(id = 'map',
-               filename = 'map screenshot')
-  })
-  
-  #observe clear selection event
-  observeEvent(input$ClearSelection, {
-    #update selected inputs
-    updateSelectizeInput(session,
-                         inputId = "selected_locations",
-                         choices = SA1$sa1_code_2021,
-                         selected = NULL,
-                         server = TRUE)
-  })
-  
-  
-  #observe selector events
-  observeEvent(input$selected_locations, {
-    removed_via_selectInput <- setdiff(selected$groups, 
-                                       input$selected_locations)
-    added_via_selectInput <- setdiff(input$selected_locations, selected$groups)
-    
-    if(length(removed_via_selectInput) > 0){
-      selected$groups <- input$selected_locations
-      proxy %>% hideGroup(group = removed_via_selectInput)
-    }
-    
-    if(length(added_via_selectInput) > 0){
-      selected$groups <- input$selected_locations
-      proxy %>% showGroup(group = added_via_selectInput)
-    }
-  }, 
-  
-  ignoreNULL = FALSE)
-  
-  #Data tables of list of SA2s
-  output$SA1.table = renderDataTable({
-    SA1 %>% 
-      filter(sa1_code_2021 %in% selected$groups) %>%
+  renderDataTable({
+    req(edits()$finished)
+    SA1_selected <- st_intersection(SA1,st_make_valid(selected$finished))
+    SA1_selected %>% 
+      # filter(sa1_code_2021 %in% selected$groups) %>%
       select('SA1 Code' = sa1_code_2021,
              #'SA2 Name' = sa2_name_2021,
              Division,
@@ -285,33 +242,16 @@ server <- function(input, output, session){
                                dom = 'Bfrtip',
                                buttons = list( 
                                  list(extend = 'csv',   
-                                      filename =  'SA2 selected'),
+                                      filename =  'SA1 selected'),
                                  list(extend = 'copy',text = 'Copy'))))
+    req(nrow(SA1_selected) > 0) 
   }, server = FALSE)
-  
-  #Data table of total
-  output$Total.table = renderDataTable({
-    SA1 %>% 
-      filter(sa1_code_2021 %in% selected$groups) %>%
-      mutate(Division = str_to_title(Division)) %>%
-      st_drop_geometry(data_all) %>%
-      summarise(tot_project = sum(tot_project)) %>%
-      mutate(deviation = paste0(round((tot_project-127238)/127238 * 100,2),
-                                '%')) %>%
-      rename('Total selected projected population' =tot_project) %>%
-      datatable(style="bootstrap",
-                selection = "multiple",
-                rownames = F,
-                extensions = 'Buttons',
-                escape=FALSE,
-                options = list(lengthChange = F,
-                               scrollX = "auto",
-                               dom = 'Bfrtip',
-                               buttons = c('copy')))
-  })
-  
 }
+  
 
 shinyApp(
   ui = ui,
   server <- server)
+    
+    
+    
